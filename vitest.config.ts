067196968e98cd7d757ea21ebc219f17460bfe19
Salva Juan <dev@salva.codes>
2026-02-04 01:43:42 +0100

    add clearMocks and globals settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
     test: {
         environment: 'happy-dom',
+        globals: false,
+        clearMocks: true,
         include: ['src/**/*.{test,spec}.{ts,tsx,js,jsx}'],
     },
 });
